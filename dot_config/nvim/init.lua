3a56e456f86f853892eb4ac9ea24e548c2774341
joe mac <jhmccarney@gmail.com>
2026-02-13 12:53:10 +0000

    Sync local changes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,28 +1,12 @@
 ---@diagnostic disable-next-line: undefined-global
 local vim = vim
 
-
-
-require 'jmac.core'
-require 'jmac.lazy'
-
+require 'config'
 
 function Transparent(color)
-  color = color or 'tokyonight'
+  color = color or 'everforest'
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 Transparent()
-
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
